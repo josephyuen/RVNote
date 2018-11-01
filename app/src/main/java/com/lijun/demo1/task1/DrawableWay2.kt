0a7a86d06f9dd73d9d74aba302e27515532b4f5e
josephyuen <josephyuenlin@gmail.com>
2018-11-01 16:52:12 +0800

    图片绘制

---
+++ app/src/main/java/com/lijun/demo1/task1/DrawableWay2.kt
@@ -0,0 +1,20 @@
+package com.lijun.demo1.task1
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Creator: yiming
+ * FuncDesc:
+ * copyright  ©2018-2020 Ququ Technology Corporation. All rights reserved.
+ */
+class DrawableWay2: AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+
+    }
+
+}
---
+++ app/src/main/java/com/lijun/demo1/task1/DrawableWay3.kt
@@ -0,0 +1,8 @@
+package com.lijun.demo1.task1
+
+/**
+ * Creator: yiming
+ * FuncDesc:
+ * copyright  ©2018-2020 Ququ Technology Corporation. All rights reserved.
+ */
+class DrawableWay3
